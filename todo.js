eb3370fe320274d9a718d191ffb09516a921d119
maurice-23 <iragaba69k@gmail.com>
2025-09-03 16:00:06 +0300

    JS commit

--- todo.js
+++ todo.js
@@ -1,1 +1,32 @@
-console.log("Hello world");
+let taskArray = [];
+let taskCount = 0;
+
+document.getElementById('form').addEventListener("submit", function(event){
+    event.preventDefault();
+    taskCount++;
+    let taskValue = document.getElementById("taskContent").value;
+    taskArray[taskArray.length] = taskValue;
+    console.log(taskArray.length);
+    console.log(taskArray)
+    renderTask();
+});
+
+function renderTask(){
+    document.getElementById("display").innerHTML = '';
+    for (let i = 0; i < taskArray.length; i++) {
+        let taskContent =  document.getElementById("display");
+        taskContent.innerHTML += `<li'>${taskArray[i]} <a href='#' onclick='deleteItem(${i})'>Delete</a></li> <br>`;
+
+    }
+
+}
+
+function deleteItem(i){
+    taskArray.splice(i,1);
+    renderTask();
+}
+
+
+
+
+
