fe2559b17aa853349753aecf6637dea7bd7e161b
maurice-23 <iragaba69k@gmail.com>
2025-09-04 17:29:30 +0300

    someting new

--- countDown.js
+++ countDown.js
@@ -1,1 +1,12 @@
-console('here for checkup');
+console.log('here for checkup');
+document.getElementById('number1').addEventListener("click",function(event){
+    event.preventDefault();
+    let myValue = document.getElementById('number1').value;
+    console.log(myValue);
+    addNumber(myValue);
+
+});
+
+function addNumber(a){
+    document.getElementById('hrs').innerHTML = a;
+}
