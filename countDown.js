5da2f83f6be114c7318aa0577366dc41dcd2ca65
maurice-23 <iragaba69k@gmail.com>
2025-09-05 22:30:12 +0300

    It passed after many hours (can add number and shift)  proud of me :)

--- countDown.js
+++ countDown.js
@@ -2,10 +2,11 @@
 let timeArray = [0,0,0,0,0,0];
 let changeTo = timeArray.length-1;
 let next = changeTo - 1;
-let bestValue;
+// let bestValue;
 
 
 function render(){
+    // console.log(timeArray);
     document.getElementById('timer').innerHTML = `${timeArray[0]}${timeArray[1]}hr ${timeArray[2]}${timeArray[3]}m ${timeArray[4]}${timeArray[5]}s`;
 }
 const keyButton = document.querySelectorAll('.keyBoard button');
@@ -13,20 +14,18 @@ const keyButton = document.querySelectorAll('.keyBoard button');
 keyButton.forEach(button => {
     button.addEventListener('click', function(event){
         event.preventDefault();
-        bestValue = this.value;
-        timeArray[changeTo] = bestValue;
-        move(bestValue);
+        move(this.value);
     })
 })
 
-function move(passValue) {
+function move(a) {
    let oldArray = [...timeArray];
    oldArray.forEach((value,i) => {
     if (i < timeArray.length - 1) {
         timeArray[i] = oldArray[i + 1];
     }
    })
-   timeArray[timeArray.length - 1] = passValue;
+   timeArray[timeArray.length - 1] = a;
    render();
 }
 // function move(a){
