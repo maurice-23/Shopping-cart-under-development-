07df1021e85949f0282f282cd9b54ba367feb704
maurice-23 <iragaba69k@gmail.com>
2025-09-05 22:35:08 +0300

    Cleaning code

--- countDown.js
+++ countDown.js
@@ -1,14 +1,6 @@
 
 let timeArray = [0,0,0,0,0,0];
-let changeTo = timeArray.length-1;
-let next = changeTo - 1;
-// let bestValue;
 
-
-function render(){
-    // console.log(timeArray);
-    document.getElementById('timer').innerHTML = `${timeArray[0]}${timeArray[1]}hr ${timeArray[2]}${timeArray[3]}m ${timeArray[4]}${timeArray[5]}s`;
-}
 const keyButton = document.querySelectorAll('.keyBoard button');
 
 keyButton.forEach(button => {
@@ -28,9 +20,7 @@ function move(a) {
    timeArray[timeArray.length - 1] = a;
    render();
 }
-// function move(a){
-//     timeArray[timeArray.length-1] = 0;
-//     timeArray[next] = a;
-//     next --;
-// }
+function render(){
+    document.getElementById('timer').innerHTML = `${timeArray[0]}${timeArray[1]}hr ${timeArray[2]}${timeArray[3]}m ${timeArray[4]}${timeArray[5]}s`;
+}
 
