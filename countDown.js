dc7cf0bec2fcfa841990468a00143931d8f83008
maurice-23 <iragaba69k@gmail.com>
2025-09-05 22:21:12 +0300

    Some logics

--- countDown.js
+++ countDown.js
@@ -1,11 +1,37 @@
 
 let timeArray = [0,0,0,0,0,0];
+let changeTo = timeArray.length-1;
+let next = changeTo - 1;
+let bestValue;
+
+
 function render(){
     document.getElementById('timer').innerHTML = `${timeArray[0]}${timeArray[1]}hr ${timeArray[2]}${timeArray[3]}m ${timeArray[4]}${timeArray[5]}s`;
 }
+const keyButton = document.querySelectorAll('.keyBoard button');
 
-// Passing value in
+keyButton.forEach(button => {
+    button.addEventListener('click', function(event){
+        event.preventDefault();
+        bestValue = this.value;
+        timeArray[changeTo] = bestValue;
+        move(bestValue);
+    })
+})
 
-function passValue(realValue) {
-    console.log(realValue);
+function move(passValue) {
+   let oldArray = [...timeArray];
+   oldArray.forEach((value,i) => {
+    if (i < timeArray.length - 1) {
+        timeArray[i] = oldArray[i + 1];
+    }
+   })
+   timeArray[timeArray.length - 1] = passValue;
+   render();
 }
+// function move(a){
+//     timeArray[timeArray.length-1] = 0;
+//     timeArray[next] = a;
+//     next --;
+// }
+
