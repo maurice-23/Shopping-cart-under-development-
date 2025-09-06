2f6f036367a11ac7e7a7b7e2da235f9cf8af8831
maurice-23 <iragaba69k@gmail.com>
2025-09-06 16:07:11 +0300

    Add delete btn function

--- countDown.js
+++ countDown.js
@@ -6,21 +6,26 @@ const keyButton = document.querySelectorAll('.keyBoard button');
 keyButton.forEach(button => {
     button.addEventListener('click', function(event){
         event.preventDefault();
-        move(this.value);
+        if (this.value == 'del') {
+            timeArray.pop();
+            timeArray.unshift(0);
+            render();
+        }else if (timeArray[0]==0) {
+            move(this.value);
+        }
     })
 })
-
 function move(a) {
-   let oldArray = [...timeArray];
-   oldArray.forEach((value,i) => {
-    if (i < timeArray.length - 1) {
-        timeArray[i] = oldArray[i + 1];
-    }
-   })
-   timeArray[timeArray.length - 1] = a;
-   render();
+    let oldArray = [...timeArray];
+    oldArray.forEach((value,i) => {
+        if (i < timeArray.length-1) {
+            timeArray[i] = oldArray[i + 1];
+        }
+    })
+    timeArray[timeArray.length - 1] = a;
+    render();
 }
 function render(){
-    document.getElementById('timer').innerHTML = `${timeArray[0]}${timeArray[1]}hr ${timeArray[2]}${timeArray[3]}m ${timeArray[4]}${timeArray[5]}s`;
+    document.getElementById('timer').innerHTML = `${timeArray[0]}${timeArray[1]}h ${timeArray[2]}${timeArray[3]}m ${timeArray[4]}${timeArray[5]}s`;
 }
 
