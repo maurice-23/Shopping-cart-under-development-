d31804f833876bc2c56aa7f63e433c90fe8f07d8
maurice-23 <iragaba69k@gmail.com>
2025-09-11 12:18:55 +0300

    Allowed to choose right answer and turn red on wrong one

--- quizApp.js
+++ quizApp.js
@@ -1,53 +1,53 @@
 let questionObj = {
-    qNumber: {question: "2+2", answer: [3,4,5,2],correct: 4}
-}
+  qNumber: { question: "2+2", answer: [3, 4, 5, 2], correct: 4 },
+};
 //Dom manipulation
 let questionNumber = 0;
-let questionBlock = document.getElementById('question');
-let optionBlock = document.getElementById('option');
-let quizForm = document.getElementById('quizForm');
+let questionBlock = document.getElementById("question");
+let optionBlock = document.getElementById("option");
+let quizForm = document.getElementById("quizForm");
 let startQuizBtn = document.getElementById("startQuiz");
 let correctAnswer = 0;
-startQuizBtn.addEventListener("click", function (event){
-    event.preventDefault();
-    questionNumber ++;
-    questionBlock.innerHTML = `${questionNumber}. ${questionObj.qNumber.question}`;
-   
-    renderOption();
-})
+startQuizBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  questionNumber++;
+  questionBlock.innerHTML = `${questionNumber}. ${questionObj.qNumber.question}`;
 
-function renderOption(){
-    questionObj.qNumber.answer.forEach((ans) =>{
-        let customid = 1;
-        optionBlock.innerHTML += `<button id='mybutton${ans}' onclick="checkMe(${ans})">${ans}</button>`
-        tmpId = 'mybutton';
-    })
-    optionBlock.innerHTML += `<button id='next'>Next</button>`;
-    
+  renderOption();
+});
+
+function renderOption() {
+  questionObj.qNumber.answer.forEach((ans) => {
+    let customid = 1;
+    optionBlock.innerHTML += `<button id='mybutton${ans}' onclick="checkMe(${ans})">${ans}</button>`;
+    tmpId = "mybutton";
+  });
+  optionBlock.innerHTML += `<button id='next'>Next</button>`;
 }
 
+function checkMe(a) {
+  optionBlock.innerHTML = " ";
+  renderOption();
+  let allButton = optionBlock.querySelector("button");
 
-function checkMe(a){
-
-    optionBlock.innerHTML = ' ';
-    renderOption();
-
-    
-    let mybutton = document.getElementById(`mybutton${a}`);
-    if (a === questionObj.qNumber.correct) {
-        console.log("Correct answer")
-        mybutton.style.backgroundColor = 'green';
-        mybutton.style.color = 'white';
-        disableButton();
-    }else{
-        console.log('wrong answer');
-        disableButton();
-    }
-    
+  let mybutton = document.getElementById(`mybutton${a}`);
+  if (a === questionObj.qNumber.correct) {
+    console.log("Correct answer");
+    mybutton.style.backgroundColor = "green";
+    mybutton.style.color = "white";
+    disableButtons();
+} else {
+    mybutton.style.backgroundColor = "red";
+    mybutton.style.color = "white";
+    console.log("wrong answer");
+    disableButtons();
+  }
 }
 
-function disableButton(){
-   let buttons = document.getElementById('option').querySelector('button');
-    buttons.disabled = true;
-
+function disableButtons() {
+  for (let i = 0; i < questionObj.qNumber.answer.length; i++) {
+    let itera = questionObj.qNumber.answer[i];
+    let button = document.getElementById(`mybutton${itera}`);
+    button.disabled = true;
+  }
 }
