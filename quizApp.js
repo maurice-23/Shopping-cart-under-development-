02082913b4a8f2daef79225488bc96d4524a6536
maurice-23 <iragaba69k@gmail.com>
2025-09-11 11:01:33 +0300

    function 1 choosing correct answer

--- quizApp.js
+++ quizApp.js
@@ -1,23 +1,39 @@
 let questionObj = {
-    question: "2+2",
-    answer: [3,4,5,2]
+    qNumber: {question: "2+2", answer: [3,4,5,2],correct: 4}
 }
 //Dom manipulation
 let questionNumber = 0;
 let questionBlock = document.getElementById('question');
 let optionBlock = document.getElementById('option');
-
+let quizForm = document.getElementById('quizForm');
 let startQuizBtn = document.getElementById("startQuiz");
 startQuizBtn.addEventListener("click", function (event){
     event.preventDefault();
     questionNumber ++;
-    questionBlock.innerHTML = `${questionNumber}. ${questionObj.question}`;
+    questionBlock.innerHTML = `${questionNumber}. ${questionObj.qNumber.question}`;
+   
     renderOption();
 })
 
+
 function renderOption(){
-    for (let i = 0; i < questionObj.answer.length; i++) {
-        optionBlock.innerHTML += `<p>${questionObj.answer[i]}</p>`;
-        
-    }
+    questionObj.qNumber.answer.forEach((ans) =>{
+        optionBlock.innerHTML += `<button onclick="checkMe(${ans})">${ans}</button>`
+    })
+    optionBlock.innerHTML += `<button>Next</button>`;
 }
+function checkMe(a){
+    optionBlock.innerHTML = ' ';
+    if (a === questionObj.qNumber.correct) {
+        console.log('Correct answer');
+    }else{
+        console.log('wrong answer');
+    }
+    renderOption();
+}
+// quizForm.addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     function checkMe(a=0){
+        
+//     }
+// })
